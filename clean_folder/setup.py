85f30642513ce75948c3825367cae7ee1cabf67f
SERHII MELNYK <melser68@i.ua>
2023-02-08 16:12:59 +0100

    Update setup.py

--- clean_folder/setup.py
+++ clean_folder/setup.py
@@ -4,7 +4,7 @@ setup(
     name='clean_folder',
     version='2',
     description='Sorting files',
-    url='http://github.com/dummy_user/useful',
+    url='https://github.com/melser68/modul_7',
     author='Serhii Melnyk',
     author_email='msprivate68@gmail.com',
     license='MIT',
