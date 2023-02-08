c74df9fb9453f7d5366a3edf6c4aba3e3fe0f519
SERHII MELNYK <melser68@i.ua>
2023-02-08 16:11:55 +0100

    packet for installation

---
+++ clean_folder/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_namespace_packages
+
+setup(
+    name='clean_folder',
+    version='2',
+    description='Sorting files',
+    url='http://github.com/dummy_user/useful',
+    author='Serhii Melnyk',
+    author_email='msprivate68@gmail.com',
+    license='MIT',
+    packages=find_namespace_packages()
+)
